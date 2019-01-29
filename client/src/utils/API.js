ad6a80d296ec976fa3b1383782c8b9b94d8e3aba
Mohammed Alzuhairi <ghazwanmohammed@yahoo.com>
2019-01-29 13:11:08 -0500

    API base url

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,45 +2,45 @@ import axios from "axios";
 
 export default {
 	getHousesByZipCode: function(zipCode) {
-		return axios.get("http://localhost:3000/api/house/zip/"+zipCode);
+		return axios.get("https://rocky-stream-14694.herokuapp.com/api/house/zip/"+zipCode);
 	},
 	getHouses: function() {
-		return axios.get("http://localhost:3000/api/houses");
+		return axios.get("https://rocky-stream-14694.herokuapp.com/api/houses");
 	},
 	getZipCodes: function() {
-		return axios.get("http://localhost:3000/api/zipcodes");
+		return axios.get("https://rocky-stream-14694.herokuapp.com/api/zipcodes");
 	},
 	login: function(username, password) {
-		return axios.post("http://localhost:3000/api/login", { username: username, password: password });
+		return axios.post("https://rocky-stream-14694.herokuapp.com/api/login", { username: username, password: password });
 	},
 	signup: function(data) {
-		return axios.post("http://localhost:3000/api/user", data);
+		return axios.post("https://rocky-stream-14694.herokuapp.com/api/user", data);
 	},
 	addHouse: function(data) {
-		return axios.post("http://localhost:3000/api/house", data);
+		return axios.post("https://rocky-stream-14694.herokuapp.com/api/house", data);
 	},
 	getHousesByUserId: function(userId) {
-		return axios.get("http://localhost:3000/api/house/user/"+userId);
+		return axios.get("https://rocky-stream-14694.herokuapp.com/api/house/user/"+userId);
 	},
 	deletHouseById: function(houseId) {
-		return axios.delete("http://localhost:3000/api/house/"+houseId);
+		return axios.delete("https://rocky-stream-14694.herokuapp.com/api/house/"+houseId);
 	},
 	getHouseById: function(houseId) {
-		return axios.get("http://localhost:3000/api/house/"+houseId);
+		return axios.get("https://rocky-stream-14694.herokuapp.com/api/house/"+houseId);
 	},
 	updateHouseById: function(data, houseId) {
-		return axios.put("http://localhost:3000/api/house/"+houseId, data);
+		return axios.put("https://rocky-stream-14694.herokuapp.com/api/house/"+houseId, data);
 	},
 	addAppt: function(data) {
-		return axios.post("http://localhost:3000/api/appointment", data);
+		return axios.post("https://rocky-stream-14694.herokuapp.com/api/appointment", data);
 	},
 	getApptByHouseId: function(houseId) {
-		return axios.get("http://localhost:3000/api/appointment/house/"+houseId);
+		return axios.get("https://rocky-stream-14694.herokuapp.com/api/appointment/house/"+houseId);
 	},
 	getApptByRenterNull: function(houseId) {
-		return axios.get("http://localhost:3000/api/appointment/house/"+houseId+"/renter");
+		return axios.get("https://rocky-stream-14694.herokuapp.com/api/appointment/house/"+houseId+"/renter");
 	},
 	rentHouse: function(id, renterUserId) {
-		return axios.put("http://localhost:3000/api/appointment/"+id, {"renterUserId": renterUserId});
+		return axios.put("https://rocky-stream-14694.herokuapp.com/api/appointment/"+id, {"renterUserId": renterUserId});
 	}
 };
