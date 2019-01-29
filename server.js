a067d0dace8e1ea6f418b54ca84a6f8ab3711952
Mohammed Alzuhairi <ghazwanmohammed@yahoo.com>
2019-01-29 12:46:11 -0500

    server.js

--- server.js
+++ server.js
@@ -9,4 +9,20 @@ app.use(express.json());
 
 require("./app/routing/apiRoutes")(app);
 
-app.listen(PORT, () => console.log(`App listing on ${PORT}`));
+var path = require('path');
+//Static file declatation
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
+app.listen(PORT, (req, res) => console.log(`App listing on ${PORT}`));
